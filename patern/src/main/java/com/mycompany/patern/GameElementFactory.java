ab23ed01fafaf9503163cc819393275fa75ec55d
Eng Taif <132089656+Taif222@users.noreply.github.com>
2024-05-04 22:53:40 +0300

    GameElementFactory.java

--- patern/src/main/java/com/mycompany/patern/GameElementFactory.java
+++ patern/src/main/java/com/mycompany/patern/GameElementFactory.java
@@ -1,4 +1,7 @@
-public interface GameElementFactory {
+
+package patternproject;
+
+public interface GameElementFactory extends GameElement{
     Player createPlayer(String name);
     Board createBoard();
     Pack createPack();
