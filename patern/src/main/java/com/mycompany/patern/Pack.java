9146fffa5fecc9ccf59caafa8916c1c72463e798
Eng Taif <132089656+Taif222@users.noreply.github.com>
2024-05-14 23:44:24 +0300

    Pack.java

--- patern/src/main/java/com/mycompany/patern/Pack.java
+++ patern/src/main/java/com/mycompany/patern/Pack.java
@@ -6,60 +6,27 @@ import java.util.Collections;
 import java.util.List;
 
 
-public class Pack implements GameElement {
-    private List<Domino> dominos;
-    private static Pack pack = null;
-
-    Pack() {
-        dominos = new ArrayList<>();
-        for (int i = 0; i <= 6; i++) {
-            for (int j = i; j <= 6; j++) {
-                dominos.add(new Domino(i, j));
-            }
-        }
-        shuffle();
-    }
-
-    public static Pack getPack() {
-        if (pack == null) {
-            pack = new Pack();
-        }
-        return pack;
-    }
-
-    public boolean isEmpty() {
-        return dominos.isEmpty();
-    }
-
-    public void shuffle() {
-        Collections.shuffle(dominos);
-    }
-
-    public Domino draw() {
-        if (!dominos.isEmpty()) {
-            return dominos.remove(0);
-        }
-        return null;
-    }
-
+public 
+class Pack implements GameElement {
     @Override
     public void start() {
-        // No implementation needed for Pack
+        System.out.println("Pack started.");
     }
 
     @Override
-    public void playRound(Player player) {
-        // No implementation needed for Pack
+    public void playRound() {
+        System.out.println("Pack played a round.");
     }
 
     @Override
     public boolean isGameFinished() {
-        // No implementation needed for Pack
         return false;
     }
 
     @Override
     public void printFinalScores() {
-        // No implementation needed for Pack
+        System.out.println("Pack scores: 0");
     }
 }
+
+
