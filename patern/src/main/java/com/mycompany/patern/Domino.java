9ff4631aa6fb8f8e7f6e95d66e584aa81045d0c3
Eng Taif <132089656+Taif222@users.noreply.github.com>
2024-05-15 19:13:24 +0300

    Domino.java

--- patern/src/main/java/com/mycompany/patern/Domino.java
+++ patern/src/main/java/com/mycompany/patern/Domino.java
@@ -1,29 +1,23 @@
 
 package patternproject;
 
-
 public class Domino {
     private int side1;
     private int side2;
     
-    //constructor
     public Domino(int side1, int side2) {
-        //Create each domino based on the values received from the pack
         this.side1 = side1;
         this.side2 = side2;
     }
     
-    //Get the Side 1 of the domino piece
     public int getSide1() {
         return side1;
     }
     
-    // Get the Side 2 of the domino piece
     public int getSide2() {
         return side2;
     }
 
-    //Print the domino piece with his two side
     @Override
     public String toString() {
         return "[" + side1 + "|" + side2 + "]";
