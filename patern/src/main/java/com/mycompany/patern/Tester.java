cbc4052635c0c1cb96473941693b9d4fb149aed8
Shifaa2002 <132432472+Shifaa2002@users.noreply.github.com>
2024-05-15 20:42:33 +0300

    Update Tester.java

--- patern/src/main/java/com/mycompany/patern/Tester.java
+++ patern/src/main/java/com/mycompany/patern/Tester.java
@@ -6,16 +6,42 @@ import java.util.List;
     
   public class Tester {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         GameElementFactory factory = new ConcreteGameElementFactory();
-        Player player1 = factory.createPlayer("Player 1");
-        Player player2 = factory.createPlayer("Player 2");
-        List<Player> players = new ArrayList<>();
-        players.add(player1);
-        players.add(player2);
-        Board board = factory.createBoard();
-        Pack pack = factory.createPack();
+        int startDecision = -1;
+        do {
+            System.out.println("Do you want to start the game? Enter 1 for yes or 0 for no");
+            String input = sc.nextLine();
 
-        Game game = new Game(players, board, pack);
-        game.start();
+            if (isValidInput(input)) {
+                startDecision = Integer.parseInt(input);
+                
+                Game game = factory.createGame(startDecision);
+
+                if (game == null) {
+                    System.out.println("See you next time");
+                } else {
+                    game.start();
+                }
+            } else {
+                System.out.println("Please enter valid entries (0 or 1)");
+            }
+
+        } while (startDecision ==- 1);
+
+        sc.close();
     }
+  
+    }
+ private static boolean isValidInput(String input) {
+        try {
+            int value = Integer.parseInt(input);
+            return value == 0 || value == 1;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        
+            
+    }
+
 }
