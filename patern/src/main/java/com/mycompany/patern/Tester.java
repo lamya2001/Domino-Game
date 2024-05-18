7f8e8c742cd604626965b0fee40e9289f6ce97ee
lamya2001 <144588377+lamya2001@users.noreply.github.com>
2024-05-18 17:07:26 +0300

    Update Tester.java

--- patern/src/main/java/com/mycompany/patern/Tester.java
+++ patern/src/main/java/com/mycompany/patern/Tester.java
@@ -4,37 +4,51 @@ import java.util.Scanner;
   public class Tester {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // Creating a Factory object to create game elements
         GameElementFactory factory = new ConcreteGameElementFactory();
+        
+        // Decision to start the game
         int startDecision = -1;
+        
+        // Loop to prompt for starting the game until a valid input is provided
         do {
-            System.out.println("Do you want to start the game? Enter 1 for yes or 0 for no");
+            System.out.println("Do you want to start the game? Enter \"1 for yes \"or  \"0 for no\"");
             String input = sc.nextLine();
-
-            if (isValidInput(input)) {
+            
+            // Check the user input
+            if (isValidInput(input)) { // If the input is valid
+                
+                // Parsing the input to an integer
                 startDecision = Integer.parseInt(input);
                 
+                // Creating the game using the Factory
                 Game game = factory.createGame(startDecision);
-
+                
+                // Checking if the game was successfully created or not
                 if (game == null) {
+                    // If the game was not created
                     System.out.println("See you next time");
                 } else {
+                    // If the game was created successfully
+                    // Starting the game
                     game.start();
                 }
             } else {
+                // If the user input is invalid
                 System.out.println("Please enter valid entries (0 or 1)");
             }
 
-        } while (startDecision ==- 1);
+        } while (startDecision ==- 1);// Looping until a valid input is obtained
 
         sc.close();
     }
   
-    
- private static boolean isValidInput(String input) {
+    // Function to check the validity of the input
+    private static boolean isValidInput(String input) {
         try {
             int value = Integer.parseInt(input);
             return value == 0 || value == 1;
-        } catch (NumberFormatException e) {
+        } catch (NumberFormatException e) {// If there's an error in parsing
             return false;
         }
         
