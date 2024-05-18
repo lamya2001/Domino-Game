2614f85321929119585938f34c50522610864352
lamya2001 <144588377+lamya2001@users.noreply.github.com>
2024-05-18 17:02:11 +0300

    Update ConcreteGameElementFactory.java

--- patern/src/main/java/com/mycompany/patern/ConcreteGameElementFactory.java
+++ patern/src/main/java/com/mycompany/patern/ConcreteGameElementFactory.java
@@ -4,35 +4,53 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ConcreteGameElementFactory implements GameElementFactory{
+    
+    //Factory Method to create a game based on the user selection 
     @Override
     public Game createGame(int startDecision){
+        
+        // If the start decision is 0, return null (do not start the game)
          if(startDecision==0){
              return null;
          }
-         else if(startDecision==1){
          
+         // If the start decision is 1, proceed to create the game
+         else if(startDecision==1){
+             
+            // Creating two players
             Player player1 = new Player("Player 1");
             Player player2 = new Player("Player 2");
+            
+            // Adding players to a list
             List<Player> players = new ArrayList<>();
             players.add(player1);
             players.add(player2);
+            
+            // Getting the singleton instance of the board
             Board board = Board.getBoard();
+            
+            // Getting the singleton instance of the pack
             Pack pack = Pack.getPack();
-
+            
+            // Creating and returning a new game instance with the players, board, and pack
             return new Game(players, board, pack);
         }
+        // If the input is not 0 or 1, return null
         return null;
     }
+    // Method to create a player with the specified name
     @Override
     public Player createPlayer(String name) {
         return new Player(name);
     }
-
+    
+    // Method to get the singleton instance of the board
     @Override
     public Board createBoard() {
         return Board.getBoard();
     }
-
+    
+    // Method to get the singleton instance of the pack
     @Override
     public Pack createPack() {
         return Pack.getPack();
