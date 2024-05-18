c693f3bda8bd11e6c43858f6a343debeff36e545
lamya2001 <144588377+lamya2001@users.noreply.github.com>
2024-05-18 21:14:53 +0300

    Update GameElementFactory.java

--- patern/src/main/java/com/mycompany/patern/GameElementFactory.java
+++ patern/src/main/java/com/mycompany/patern/GameElementFactory.java
@@ -1,8 +1,1 @@
-package patternproject;
 
-public interface GameElementFactory {
-    Player createPlayer(String name);
-    Board createBoard();
-    Pack createPack();
-    Game createGame(int startDecision);
-}
